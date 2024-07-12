a11e49689ef8e27c937b8e3a814fb7bb79bd6d38
AhmadSaleh99 <saleehsale816@gmail.com>
2024-07-12 15:01:35 -0500

    clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import React from "react";
+import "../globals.css";
+
+export const metadata = {
+  title: "Threads",
+  description: "A next-js 13 metadata Threads application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Rootlayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
