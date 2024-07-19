c75b96ebfc756abd4cac0e5cb78eedf04bcb1227
AhmadSaleh99 <saleehsale816@gmail.com>
2024-07-19 02:24:03 -0500

    aa

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -126,3 +126,30 @@ export async function fetchUsers({
     throw new Error(`failed to fetch users: ${error.message}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    const userThreads = await Thread.find({ author: userId });
+
+    // collect all the user thread ids (replies or children)
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Faild to fetch activity ${error.message}`);
+  }
+}
